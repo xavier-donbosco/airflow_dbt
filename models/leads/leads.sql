7f7b33d69d96db9ed6456dc6e41a0fecde9b6dcb
Xavier Bosco <xavier.bosco@electrolux.com>
2023-03-07 19:44:17 +0530

    initial commit

--- models/leads/leads.sql
+++ models/leads/leads.sql
@@ -3,4 +3,5 @@
     file_format = 'delta',
     schema='airflow'
 )}}
+
 select AccountCity, AccountCountry, AccountCountryText, AccountPartyID, AccountPartyName, AccountPostalAddressElementsStreetName, AccountState, AccountStateText, Company from airflow.leads_bronze
